a44aad8ebe52f9f0e2c2c4dfc9a6dcaaaad02f24
Shamin Asfaq <shamin.asfaq@gmail.com>
2019-11-07 17:45:03 +0600

    Minor update in script.js

--- script.js
+++ script.js
@@ -16,37 +16,41 @@ app.use(function(req, res, next) {
 
 var list = [];
 app.get('/', (req, res) => {
-	fs.createReadStream('ICPC Dhaka 2019 collecting photos (Responses) - Form Responses 1.csv')
-	.pipe(csv())
-	.on('data', (data) => {
 
-		var institute = 'Name of the University';
-		var team_name = 'Name of the team';
-		var coach_name = 'Name of the coach';
-		var c1_name = 'Name of contestant 1';
-		var c2_name = 'Name of contestant 2';
-		var c3_name = 'Name of contestant 3';
-		var cm_id = 'CM ID';
+	if(list.length===0){
+		fs.createReadStream('ICPC Dhaka 2019 collecting photos (Responses) - Form Responses 1.csv')
+		.pipe(csv())
+		.on('data', (data) => {
+		    
+		    var institute = 'Name of the University';
+		    var team_name = 'Name of the team';
+		    var coach_name = 'Name of the coach';
+		    var c1_name = 'Name of contestant 1';
+		    var c2_name = 'Name of contestant 2';
+		    var c3_name = 'Name of contestant 3';
+		    var cm_id = 'CM ID';
 
-		list.push({
-			institute: data[institute],
-			team_name: data[team_name],
-			coach_name: data[coach_name],
-			c1_name: data[c1_name],
-			c2_name: data[c2_name],
-			c3_name: data[c3_name],
-			cm_id: data[cm_id]
+		    list.push({
+		        institute: data[institute],
+		        team_name: data[team_name],
+		        coach_name: data[coach_name],
+		        c1_name: data[c1_name],
+		        c2_name: data[c2_name],
+		        c3_name: data[c3_name],
+		        cm_id: data[cm_id]
+		    });
+		})
+		.on('end', () => {
+
+		    list = list.sort((a,b) => {
+		        return a.institute > b.institute ? 1 : -1
+		    });
+
+		    res.send(list);
 		});
-	})
-	.on('end', () => {
-
-		list = list.sort((a,b) => {
-			return a.institute > b.institute ? 1 : -1
-		});
-
-		res.send(list);
-		list = []
-	});
+    } else {
+		res.send(list)
+	}
 });
 
 app.get('/url', (req, res) => {
